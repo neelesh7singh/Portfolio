83b66f0ad55380e0473addd66a728e718e1a243b
Neelesh <neeleshsingh767@gmail.com>
2020-11-17 18:49:44 +0530

    added all animation for homepage

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Works from './components/works.jsx';
 import Skill from './components/skill.jsx';
 import Contact from './components/contact.jsx';
 import useWindowSize from './useWindowSize.js';
+import Particles from 'react-particles-js';
 
 
 
@@ -60,6 +61,118 @@ function App() {
 
   return (
     <div ref={app} className="App">
+      <Particles
+        className="particles"
+        params={{
+          "particles": {
+            "number": {
+              "value": 40,
+              "density": {
+                "enable": true,
+                "value_area": 1500
+              }
+            },
+            "color": {
+              "value": "#ffffff"
+            },
+            "shape": {
+              "type": "circle",
+              "stroke": {
+                "width": 0,
+                "color": "#000000"
+              },
+              "polygon": {
+                "nb_sides": 5
+              },
+              "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+              }
+            },
+            "opacity": {
+              "value": 1,
+              "random": true,
+              "anim": {
+                "enable": true,
+                "speed": 1,
+                "opacity_min": 0,
+                "sync": false
+              }
+            },
+            "size": {
+              "value": 2,
+              "random": true,
+              "anim": {
+                "enable": false,
+                "speed": 4,
+                "size_min": 0.3,
+                "sync": false
+              }
+            },
+            "line_linked": {
+              "enable": false,
+              "distance": 150,
+              "color": "#ffffff",
+              "opacity": 0.4,
+              "width": 1
+            },
+            "move": {
+              "enable": true,
+              "speed": 1,
+              "direction": "none",
+              "random": true,
+              "straight": false,
+              "out_mode": "out",
+              "bounce": false,
+              "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 600
+              }
+            }
+          },
+          "interactivity": {
+            "detect_on": "window",
+            "events": {
+              "onhover": {
+                "enable": true,
+                "mode": "bubble"
+              },
+              "onclick": {
+                "enable": true,
+                "mode": "repulse"
+              },
+              "resize": true
+            },
+            "modes": {
+              "grab": {
+                "distance": 400,
+                "line_linked": {
+                  "opacity": 1
+                }
+              },
+              "bubble": {
+                "distance": 250,
+                "size": 0,
+                "duration": 2,
+                "opacity": 0,
+                "speed": 3
+              },
+              "repulse": {
+                "distance": 400,
+                "duration": 0.4
+              },
+              "push": {
+                "particles_nb": 4
+              },
+              "remove": {
+                "particles_nb": 2
+              }
+            }
+          },
+          "retina_detect": true
+        }} />
       <div className="cursor" ref={(el) => (c = el)} />
       <div ref={scrollContainer} className="scroll">
         <Banner />
@@ -68,7 +181,7 @@ function App() {
         <Skill />
         <Contact />
       </div>
-    </div>
+    </div >
   );
 }
 
