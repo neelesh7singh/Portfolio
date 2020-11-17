cc008af82c8f45ebf7c43a3586d272bf490b9a0d
Neelesh <neeleshsingh767@gmail.com>
2020-11-17 18:55:41 +0530

    Updates

---
+++ precache-manifest.1121e4e521a197035c8e2bc3eff1109d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0c3bb95f01aaf18c67dc62ac2ac347",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d5a13d2e69a598c9c229",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "da16d212a214c8c8a1f0",
+    "url": "/portfolio/static/css/main.86cd526d.chunk.css"
+  },
+  {
+    "revision": "d5a13d2e69a598c9c229",
+    "url": "/portfolio/static/js/2.a44a3bca.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/portfolio/static/js/2.a44a3bca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da16d212a214c8c8a1f0",
+    "url": "/portfolio/static/js/main.d76326a5.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
