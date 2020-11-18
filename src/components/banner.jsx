9df4c5d12cb5625590502c51e8ef2b125b5d6c5d
Neelesh <neeleshsingh767@gmail.com>
2020-11-18 20:44:44 +0530

    seperated background as separate component

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -7,13 +7,8 @@ const Banner = () => {
     let h = useRef(null);
     let i = useRef(null);
     let portfolio = useRef(null);
-    // let line = useRef(null);
-    // let loader = useRef(null);
     useEffect(() => {
         TweenMax.from(portfolio, 1.5, {y: -100, opacity: 0, ease: "power4.out", delay:3.4});
-        // TweenMax.from(line, 2, {x: '-100%'});
-        // TweenMax.to(loader, 1, {opacity: 0, display: 'none', delay:2});
-        // TweenMax.to(line, 1, {opacity: 0, display: 'none', delay:2});
         TweenMax.staggerFrom(
             [h, i],
             1,
@@ -23,9 +18,6 @@ const Banner = () => {
     },[])
     return (
         <>
-        {/* <div className = 'loader' ref = {(el) => (loader = el)}>
-            <div className="line" ref = {(el) => (line = el)}/>
-        </div> */}
         <div className = 'banner'>
         <div className = "top-bar" ref = {(el) => (portfolio = el)}>
             <p >Portfolio.</p>
