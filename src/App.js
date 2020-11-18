55ef4359a1cc653e8c25391397a87171a649b8a1
Neelesh <neeleshsingh767@gmail.com>
2020-11-18 20:30:17 +0530

    changed file structure of the project

--- src/App.js
+++ src/App.js
@@ -1,67 +1,12 @@
-import React, { useRef, useEffect } from 'react';
-import Cursor from './cursor';
+import React from 'react';
 import './styles/App.scss';
-import Banner from './components/banner.jsx'
-import Intro from './components/intro.jsx';
-import Works from './components/works.jsx';
-import Skill from './components/skill.jsx';
-import Contact from './components/contact.jsx';
-import Preloader from './components/preloader.jsx';
-import useWindowSize from './useWindowSize.js';
 import Particles from 'react-particles-js';
-
-
+import HomePage from './pages/homePage';
+import { Switch, Route } from "react-router-dom";
 
 function App() {
-
-  const size = useWindowSize();
-
-  let c = useRef(null);
-  const app = useRef(null);
-  const scrollContainer = useRef(null);
-
-  useEffect(() => {
-    new Cursor(c);
-  }, [])
-
-  const skewConfigs = {
-    ease: 0.1,
-    current: 0,
-    previous: 0,
-    rounded: 0
-  };
-
-  useEffect(() => {
-    requestAnimationFrame(() => skewScrolling());
-  });
-
-  useEffect(() => {
-    document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`
-  }, [size.height])
-
-  const skewScrolling = () => {
-    //Set Current to the scroll position amount
-    skewConfigs.current = window.scrollY;
-    // Set Previous to the scroll previous position
-    skewConfigs.previous += (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
-    // Set rounded to
-    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
-
-    // Difference between
-    const difference = skewConfigs.current - skewConfigs.rounded;
-    const acceleration = difference / size.width;
-    const velocity = +acceleration;
-    const skew = velocity * 7.7;
-
-    //Assign skew and smooth scrolling to the scroll container
-    scrollContainer.current.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0) skewY(${skew}deg)`;
-
-    //loop vai raf
-    requestAnimationFrame(() => skewScrolling());
-  }
-
   return (
-    <div ref={app} className="App">
+    <>
       <Particles
         className="particles"
         params={{
@@ -174,16 +119,11 @@ function App() {
           },
           "retina_detect": true
         }} />
-      <Preloader />
-      <div className="cursor" ref={(el) => (c = el)} />
-      <div ref={scrollContainer} className="scroll">
-        <Banner />
-        <Intro />
-        <Works />
-        <Skill />
-        <Contact />
-      </div>
-    </div >
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,73 @@
+import React, { useRef, useEffect } from 'react'
+import Banner from '../components/banner.jsx'
+import Intro from '../components/intro.jsx';
+import Works from '../components/works.jsx';
+import Skill from '../components/skill.jsx';
+import Contact from '../components/contact.jsx';
+import Preloader from '../components/preloader.jsx';
+import useWindowSize from '../useWindowSize.js';
+import Cursor from '../cursor';
+
+function HomePage() {
+
+  const size = useWindowSize();
+
+  let c = useRef(null);
+  const app = useRef(null);
+  const scrollContainer = useRef(null);
+
+  useEffect(() => {
+    new Cursor(c);
+  }, [])
+
+  const skewConfigs = {
+    ease: 0.1,
+    current: 0,
+    previous: 0,
+    rounded: 0
+  };
+
+  useEffect(() => {
+    requestAnimationFrame(() => skewScrolling());
+  });
+
+  useEffect(() => {
+    document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`
+  }, [size.height])
+
+  const skewScrolling = () => {
+    //Set Current to the scroll position amount
+    skewConfigs.current = window.scrollY;
+    // Set Previous to the scroll previous position
+    skewConfigs.previous += (skewConfigs.current - skewConfigs.previous) * skewConfigs.ease;
+    // Set rounded to
+    skewConfigs.rounded = Math.round(skewConfigs.previous * 100) / 100;
+
+    // Difference between
+    const difference = skewConfigs.current - skewConfigs.rounded;
+    const acceleration = difference / size.width;
+    const velocity = +acceleration;
+    const skew = velocity * 7.7;
+
+    //Assign skew and smooth scrolling to the scroll container
+    scrollContainer.current.style.transform = `translate3d(0, -${skewConfigs.rounded}px, 0) skewY(${skew}deg)`;
+
+    //loop vai raf
+    requestAnimationFrame(() => skewScrolling());
+  }
+    return (
+        <div ref={app} className="App">
+      <Preloader />
+      <div className="cursor" ref={(el) => (c = el)} />
+      <div ref={scrollContainer} className="scroll">
+        <Banner />
+        <Intro />
+        <Works />
+        <Skill />
+        <Contact />
+      </div>
+    </div >
+    )
+}
+
+export default HomePage
