f6014db8f732595fd7876777dce260991b318e26
Neelesh <neeleshsingh767@gmail.com>
2020-11-18 16:13:07 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.640d2a0e723f830db6e827fa7803ae23.js"
+  "/portfolio/precache-manifest.e9644c6255616b2e76fd47461cb601eb.js"
 );
 
 self.addEventListener('message', (event) => {
